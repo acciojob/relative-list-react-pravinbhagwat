52a4b1a4bfbb1763224033a08cd03c68163ce46f
pravinbhagwat <pravinbhagwat150@gmail.com>
2023-06-27 15:22:10 +0530

    submitting code-Tue Jun 27 2023 15:22:09 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+        <ol id='relativeList'>
+                {
+                  list.map((name, indx)=>(
+                    <li id={`relativeListItem${indx+1}`}>{name}</li>
+                  ))
+                }
+               </ol>
     </div>
   )
 }
